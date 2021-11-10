b295f1d8ec8141c39486c001b531bb68ee80c3ae
farukumarx64 <farukumaridris2002@gmail.com>
2021-11-10 15:54:01 +0100

    Updated the App

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(const DeliveryApp());
@@ -18,84 +18,4 @@ class DeliveryApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  var _currentIndex = 0;
-  var barColor = 0xFF43A047;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('AUI DELIVERY'),
-        foregroundColor: Color(barColor),
-        backgroundColor: Colors.white,
-      ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                width: 300,
-                height: 60,
-                child: HomeBar(),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(60),
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 2,
-                        blurRadius: 2,
-                        offset: const Offset(0, 2))
-                  ],
-                ),
-              ),
-              const SizedBox(height: 10)
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
-  SalomonBottomBar HomeBar() {
-    return SalomonBottomBar(
-      currentIndex: _currentIndex,
-      onTap: (i) => setState(() => _currentIndex = i),
-      items: [
-        /// Home
-        SalomonBottomBarItem(
-          icon: Icon(Icons.fastfood_outlined),
-          activeIcon: Icon(Icons.fastfood),
-          title: Text("Order"),
-          selectedColor: Color(barColor),
-        ),
-
-        /// Likes
-        SalomonBottomBarItem(
-          icon: Icon(Icons.delivery_dining_outlined),
-          activeIcon: Icon(Icons.delivery_dining),
-          title: Text("Deliver"),
-          selectedColor: Color(barColor),
-        ),
-
-        /// Profile
-        SalomonBottomBarItem(
-          icon: Icon(Icons.person_outlined),
-          activeIcon: Icon(Icons.person),
-          title: Text("Profile Settings"),
-          selectedColor: Color(barColor),
-        ),
-      ],
-    );
-  }
-}
